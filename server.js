31966c79123b1c092175021600fa2980569b6960
Dmitry Dyachenko <ddyachenko@golin.com>
2017-10-23 21:27:27 +0100

    feat(*): Bot added

--- server.js
+++ server.js
@@ -4,23 +4,24 @@
 // =============================================================================
 
 // call the packages we need
-var express = require('express');       
-var cors = require('cors');       
+var express = require('express');
+var builder = require('botbuilder');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 8080;   
+var port = process.env.PORT || 3978;
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-var app = express();   
+var app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();             
+var router = express.Router();
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
@@ -33,8 +34,24 @@ router.get('/', function (req, res) {
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
+// REGISTER OUR BOT -------------------------------
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '88d62595-abc7-4f4e-afa3-a7d2dd704100',
+    appPassword: 'iAPeEvqqeyKjofbEPqa3tjK'
+});
+
+var bot = new builder.UniversalBot(connector);
+
+app.post('/api/messages', connector.listen());
+
+bot.dialog('/', function (session, args) {
+    session.send("Hi");
+    console.log(session.message.text);
+});
+
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-
-console.log('Magic happens on port ' + port);
+app.listen(port, function () {
+    console.log('Web Server listening on port %s', port);
+});
