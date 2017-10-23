03f26ec2165495fa612ef2b2afa6bd293a9fe7ad
Dmitry Dyachenko <ddyachenko@golin.com>
2017-10-23 17:44:57 +0100

    feat(*): Updates

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var router = express.Router();
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'Welcome to our api!' });
 });
 
 // more routes for our API will happen here
